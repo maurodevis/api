c4f48fd5314c3e35d487d9da51f6ad6538461025
maurodevis <mauricio.aponte.2733@gmail.com>
2024-02-09 17:35:45 -0500

    se hicieron cositas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
+   const [showdata, setShow] = useState(null)
+  
+   console.log(data)
+   
+  const newData =  async () => {
+   
+    const res =  await data.data
+    
+    setShow(res)
+  }
+
 
   useEffect(() => {
+
+    newData()
     const fetchData = async () => {
       try {
         const response = await fetch('https://www.finanzauto.info/Carfiao.Inventario/api/Vehicle/GetVehiclesByFilters?PageIndex=10&PageSize=1');
-        const jsonData = await response.json();
-        console.log()
-        setData(jsonData);
+        const jsonData = await response.json(); 
+
+       
+        
+        setData(jsonData.data);
         
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -17,15 +32,26 @@ const App = () => {
     };
 
     fetchData();
-  }, ); // El segundo parámetro [] asegura que useEffect se ejecute solo una vez al montar el componente.
+  }, []); 
 
   return (
     <div>
-    {data ? (
+    {/* {data ? (
       <pre>{JSON.stringify(data, null, 2)}</pre>
     ) : (
       <p>Cargando datos...</p>
-    )}
+    )} */}
+    {data && data.map((dato) => (
+      <ul>
+        <li>Marca de vehiculo:   {dato.brand}</li>
+        <li>Año de vehiculo {dato.model} </li>
+        <li>Kilometraje de vehiculo: {dato.mileage} </li>
+        <li>Placa del vehiculo: {dato.licencePlate} </li>
+        <img src={dato.routeImageMain} alt="imgen vehiculo" />
+      </ul>
+    )
+    )} 
+    <p></p>
   </div>
   );
 };
